ad4f1d91854612e6df7765e5ecb2bab2c1820bb1
ashishpatel17 <mpatelashish@gmail.com>
2026-03-17 22:28:56 +0530

    deploy on github pages

--- react-pokemon-incubyte/src/App.tsx
+++ react-pokemon-incubyte/src/App.tsx
@@ -1,17 +1,17 @@
 import { Container, Typography } from "@mui/material";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import PokemonList from "./pages/PokemonList";
 import PokemonDetail from "./pages/PokemonDetail";
 
 export default function App() {
   return (
-    <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+    <HashRouter future={{ v7_relativeSplatPath: true }}>
       <Container maxWidth={false} disableGutters sx={{ width: "100%" }}>
         <Routes>
           <Route path="/" element={<PokemonList />} />
           <Route path="/detail/:pokemonId" element={<PokemonDetail />} />
         </Routes>
       </Container>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
--- react-pokemon-incubyte/vite.config.ts
+++ react-pokemon-incubyte/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
 
   test: {
